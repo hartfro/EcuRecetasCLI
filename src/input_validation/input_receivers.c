3a1c7d6a8cad64817cd6020c62c7f77a99b4d44b
Anthony Suárez <neolight1010@gmail.com>
2021-05-16 01:06:21 -0500

    Remove unnecesary docs in input_receivers.c

--- src/input_validation/input_receivers.c
+++ src/input_validation/input_receivers.c
@@ -5,12 +5,6 @@
 #define INPUT_ERROR_MSG "\nValor inválido. Intente de nuevo.\n"
 #define BUFFER_SIZE 100
 
-/**
- * Recibe input de tipo int del usuario por teclado y no permite continuar hasta
- * que este sea válido.
- * @param *x es la variable que guardará el input del usuario.
- * @param *validator es una función que devuelve true si el input es correcto.
- */
 void int_input(int *x, bool (*validator)(int a)) {
   char input_str[BUFFER_SIZE];
   int input;
