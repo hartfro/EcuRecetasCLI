e48fb75a70a370202b0e544c939895a2cd1c77cc
Anthony Suárez <neolight1010@gmail.com>
2021-05-16 22:16:35 -0500

    Refactor cli_crear_receta with valid. input funcs.

--- src/cli_options.c
+++ src/cli_options.c
@@ -1,3 +1,5 @@
+#include <input_validation/input_receivers.h>
+#include <input_validation/validators.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,60 +9,26 @@ void clear_screen() { printf("\e[1;1H\e[2J"); }
 
 void cli_crear_receta() {
   int number_of_ingredients, number_of_instructions;
-  char quantity_of_ingredient[BUFFER_SIZE],
-      number_of_ingredients_str[BUFFER_SIZE], recipe_name[BUFFER_SIZE],
-      ingredient_name[BUFFER_SIZE], instruction[BUFFER_SIZE],
-      measurement[BUFFER_SIZE], number_of_instructions_str[BUFFER_SIZE];
+  float quantity_of_ingredient;
+  char recipe_name[BUFFER_SIZE], ingredient_name[BUFFER_SIZE],
+      instruction[BUFFER_SIZE];
 
   fputs("Ingrese el nombre de la receta: ", stdout);
   fgets(recipe_name, BUFFER_SIZE, stdin);
 
-  while (1) {
-    fputs("Ingrese la cantidad de ingredientes: ", stdout);
-    fgets(number_of_ingredients_str, BUFFER_SIZE, stdin);
-
-    number_of_ingredients = atoi(number_of_ingredients_str);
-
-    if (number_of_ingredients <= 0) {
-      printf("Cantidad incorrecta. Intente de nuevo.\n");
-      continue;
-    }
-
-    break;
-  }
+  fputs("Ingrese la cantidad de ingredientes", stdout);
+  int_input(&number_of_ingredients, &validate_int_g0);
 
   for (int i = 1; i <= number_of_ingredients; i++) {
     fputs("\nIngrese el nombre del ingrediente: ", stdout);
     fgets(ingredient_name, BUFFER_SIZE, stdin);
 
-    while (1) {
-      fputs("Ingrese la cantidad del ingrediente: ", stdout);
-      fgets(quantity_of_ingredient, BUFFER_SIZE, stdin);
-
-      float quantity_of_ingredientFloat = atof(quantity_of_ingredient);
-
-      if (quantity_of_ingredient <= 0) {
-        printf("Cantidad incorrecta. Intente de nuevo.\n");
-        continue;
-      }
-
-      break;
-    }
+    fputs("Ingrese la cantidad del ingrediente", stdout);
+    float_input(&quantity_of_ingredient, &validate_float_g0);
   }
 
-  while (1) {
-    fputs("\nIngrese la cantidad de instrucciones: ", stdout);
-    fgets(number_of_instructions_str, BUFFER_SIZE, stdin);
-
-    number_of_instructions = atoi(number_of_instructions_str);
-
-    if (number_of_instructions <= 0) {
-      printf("Cantidad incorrecta. Intente de nuevo.\n");
-      continue;
-    }
-
-    break;
-  }
+  fputs("\nIngrese la cantidad de instrucciones", stdout);
+  int_input(&number_of_instructions, &validate_int_g0);
 
   for (int i = 1; i <= number_of_instructions; i++) {
     fputs("\nIngrese cada instrucción para la receta: ", stdout);
