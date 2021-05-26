a4609f2dd67d0a4239922229594886647ac09f33
Anthony Suárez <neolight1010@gmail.com>
2021-05-26 17:41:43 -0500

    Add non-color styles to style.h

--- headers/style.h
+++ headers/style.h
@@ -2,6 +2,14 @@
 #define COLORS_H
 
 #define ESC "\x1B"
+#define RESET ESC "[0m"
+
+#define BLD ESC "[1m"
+#define DIM ESC "[2m"
+#define ITL ESC "[3m"
+#define UNDR ESC "[4m"
+#define BLNK ESC "[5m"
+#define STRK ESC "[9m"
 
 #define RED ESC "[31m"
 #define GRN ESC "[32m"
@@ -10,6 +18,5 @@
 #define MAG ESC "[35m"
 #define CYN ESC "[36m"
 #define WHT ESC "[37m"
-#define RESET ESC "[0m"
 
 #endif
