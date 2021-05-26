88804ab9cbbb7a37f30aa46ae89549c48271b2a2
Anthony Suárez <neolight1010@gmail.com>
2021-05-26 17:31:01 -0500

    Rename colors.h to style.h

---
+++ headers/style.h
@@ -0,0 +1,15 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define ESC "\x1B"
+
+#define RED ESC "[31m"
+#define GRN ESC "[32m"
+#define YEL ESC "[33m"
+#define BLU ESC "[34m"
+#define MAG ESC "[35m"
+#define CYN ESC "[36m"
+#define WHT ESC "[37m"
+#define RESET ESC "[0m"
+
+#endif
