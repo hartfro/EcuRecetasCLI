83cc0e5d1a19f65e22895932b02ecf3c0b26a65d
Anthony Suárez <neolight1010@gmail.com>
2021-06-27 20:12:44 -0500

    Add user confirmations to cli_modificar_receta.

--- src/cli_options/cli_modificar_receta.c
+++ src/cli_options/cli_modificar_receta.c
@@ -3,6 +3,7 @@
 #include <input_validation/input_receivers.h>
 #include <input_validation/validators.h>
 #include <stdio.h>
+#include <string.h>
 #include <style.h>
 
 void cli_modificar_receta(
@@ -27,26 +28,53 @@ void cli_modificar_receta(
 
   if (i >= 0) {
     // If recipe was found.
-    fputs(BLD "Ingrese el nuevo nombre de la receta: " RESET, stdout);
+    fputs(BLD "Ingrese el nuevo nombre de la receta (en blanco para no "
+              "modificar): " RESET,
+          stdout);
     fgets(new_name, BUFFER_SIZE, stdin);
 
-    fputs(BLD "Ingrese el nuevo número de ingredientes" RESET, stdout);
-    int_input(&new_n_ingredients, &validate_int_g0);
-
-    for (int j = 0; j < new_n_ingredients; j++) {
-      fputs(BLD "\nIngrese el nombre del ingrediente: " RESET, stdout);
-      fgets(new_ingredients[j], BUFFER_SIZE, stdin);
-
-      fputs(BLD "Ingrese la cantidad del ingrediente" RESET, stdout);
-      float_input(&new_quantities_of_ingredients[j], &validate_float_g0);
+    if (strcmp(new_name, "\n")) {
+      strcpy(new_name, recipe_names[i]);
     }
 
-    fputs(BLD "\nIngrese la nueva cantidad de instrucciones" RESET, stdout);
-    int_input(&new_n_instructions, &validate_int_g0);
+    char s_n_str[BUFFER_SIZE];
+    puts(BLU BLD "¿Modificar ingredientes? (s/n)" RESET);
+    fgets(s_n_str, BUFFER_SIZE, stdin);
 
-    for (int j = 0; j < new_n_instructions; j++) {
-      fputs(BLD "\nIngrese cada instrucción para la receta: " RESET, stdout);
-      fgets(new_instructions[j], BUFFER_SIZE, stdin);
+    if (strcmp(s_n_str, "s\n") == 0) {
+      fputs(BLD "Ingrese el nuevo número de ingredientes (en blanco para no "
+                "modificar)" RESET,
+            stdout);
+      int_input(&new_n_ingredients, &validate_int_g0);
+
+      for (int j = 0; j < new_n_ingredients; j++) {
+        fputs(BLD "\nIngrese el nombre del ingrediente: " RESET, stdout);
+        fgets(new_ingredients[j], BUFFER_SIZE, stdin);
+
+        fputs(BLD "Ingrese la cantidad del ingrediente" RESET, stdout);
+        float_input(&new_quantities_of_ingredients[j], &validate_float_g0);
+      }
+    } else {
+      new_n_ingredients = n_ingredients[i];
+      memcpy(new_ingredients, ingredient_names[i], sizeof new_ingredients);
+      memcpy(new_quantities_of_ingredients, quantities_of_ingredients[i],
+             sizeof new_quantities_of_ingredients);
+    }
+
+    puts(BLU BLD "¿Modificar instrucciones? (s/n)" RESET);
+    fgets(s_n_str, BUFFER_SIZE, stdin);
+
+    if (strcmp(s_n_str, "s\n") == 0) {
+      fputs(BLD "\nIngrese la nueva cantidad de instrucciones" RESET, stdout);
+      int_input(&new_n_instructions, &validate_int_g0);
+
+      for (int j = 0; j < new_n_instructions; j++) {
+        fputs(BLD "\nIngrese cada instrucción para la receta: " RESET, stdout);
+        fgets(new_instructions[j], BUFFER_SIZE, stdin);
+      }
+    } else {
+      new_n_instructions = n_instructions[i];
+      memcpy(new_instructions, instructions[i], sizeof new_instructions);
     }
 
     modify_recipe(i, new_name, new_n_ingredients, new_ingredients,
